86d60e64f2dc7bad206c80d831ea299cb042275b
HeberIniguez <126428820+HeberIniguez@users.noreply.github.com>
2025-03-25 18:25:05 -0700

    create login/register

---
+++ config/server.js
@@ -0,0 +1,77 @@
+import crypto from 'node:crypto';
+
+import bcrypt from 'bcrypt';
+import DBLocal from 'db-local';
+
+const { Schema } = new DBLocal({ path: '../db'});
+
+// Creacion del schema de usuarios
+const User = Schema('Users', {
+    _id: { type: String, require: true },
+    username: { type: String, require: true },
+    password: { type: String, require: true },
+    email: { type: String, requiere: true},
+})
+
+// Validaciones (Probar la biblioteca zod despues)
+class Validation {
+    static username (username) {
+    // Validar nombres de usuarios 
+    if (typeof username !== 'string') throw new Error('El usuario tiene que ser una cadena de texto.');
+    if (username.length < 3) throw new Error('El usuario tiene que ser mayor a 3 caracteres.');
+    }
+
+    static password (password) {
+    // Validar la contraseña
+    if(typeof password !== 'string') throw new Error('La contraseña tiene que ser una cadena de texto.');
+    if(password.length < 8) throw new Error('La contraseña tiene que tener mayor a 8 caracteres.');
+    }
+
+    static email (email) {
+        // Validar email
+        const validate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!(validate.test(email))) throw new Error('El correo no es correcto.');
+    }
+}
+
+export class registerUser {
+    static async create ({username, password, email}) {
+        Validation.username(username);
+        Validation.password(password);
+        Validation.password(email);
+
+        // Validar que no se repitan los usuarios
+        const user = User.findOne({ username });
+        if (user) throw new Error('El usuario ya existe.');
+
+        // Generacion del ID con randomUUID
+        const id = crypto.randomUUID();
+
+        //Encriptar contraseña
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        User.create({
+            _id: id,
+            username,
+            password: hashedPassword,
+            email
+        }).save();
+
+        return id;
+    }
+
+    static async login ({username, password}) {
+        Validation.username(username);
+        Validation.password(password);
+        const user = User.findOne({ username });
+        if ( !user ) throw new Error('El usuario no existe.');
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) throw new Error('La contraseña es incorrecta.');
+
+        const { password: _, ... publicUser } = user;
+
+        return publicUser;
+    }
+}
--- index.js
+++ index.js
@@ -1,25 +1,87 @@
 // Importamos las dependencias
 import express from 'express';
-import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser';
 
-// fix para __dirname
-import path from 'path';
-import { fileURLToPath } from 'node:url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { registerUser } from './config/server.js';
 
-// Determina que enviorement cargar.
-const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production';
-dotenv.config({ path: path.resolve(__dirname, `../${envFile}`) });
-
-const PORT = process.env.PORT || 8000;
+const PORT = 8080;
 
 // Framework de aplicaciones web
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// recuperar la session y pasar a la siguiente ruta
+app.use((req, res, next) => {
+    const token = req.cookies.access_token;
+    req.session = { user: null };
+
+    try {
+        const data = jwt.verify(token, 'jsonwebtoken_super_secreto');
+        req.session.user = data;
+    } catch {}
+
+    next();
+})
+
 // Al recibir una peticion GET del navegador, enviamos como respuesta "Bienvenido"
 app.get('/', (req, res) => {
-    res.send('<h1> Bienvenido </h1>');
+    const { user } = req.session;
+    if ( !user ) res.send('<h1> Bienvenido al Proyecto Integrador </h1>');
+    res.send(`<h1> Hola ${ user.username } </h1>`);
 })
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    
+    try{
+        const user = await registerUser.login({ username, password });
+        const token = jwt.sign({ user: user._id, username: user.username }, 
+        'jsonwebtoken_super_secreto', {
+            expiresIn: '1h',
+        })
+
+        res
+        .cookie('access_token', token, {
+            httpOnly: true, // cookie solo accesible desde html
+            maxAge: 1000 * 60 * 60
+        })
+        .send({ user, token })
+
+    }catch (error) {
+        res.status(401).send(error.message);
+    }
+});
+
+// Permite registrar un nuevo usuario
+app.post('/register', async (req, res) => {
+    const {username, password, email } = req.body;
+    console.log({ username, password, email });
+
+    try { 
+        const id = await registerUser.create({ username, password, email });
+        res.send({ id, username });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
+
+// Elimina la cookie cerrando la session
+app.post('/logout', (req, res) => {
+    res
+    .clearCookie('access_token')
+    .json({ message: 'Ha cerrado su session'})
+});
+
+//Solo accesible si se ha iniciado session.
+app.get('/protected', (req, res) => {
+    const { user } = req.session;
+    if (!user) return res.status(403).send('Acceso no autorizado');
+    res.send(`<h1> Pagina protegida de ${ user.username } </h1>`);
+});
 
 // Muestra por consola la direccion donde esta corriendo el servidor.
 app.listen(PORT, () => {
